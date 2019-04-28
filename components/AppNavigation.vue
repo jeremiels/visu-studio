469324023d7461dbfdcf4982b898697c4119b0ab
Jérémie <jeremie.ls@gmail.com>
2019-04-28 13:03:38 +0200

    reorder style

--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -1,15 +1,27 @@
 <template>
   <nav>
     <ul class="nav__list">
-      <li class="nav__list-item">
-        <nuxt-link class="nuxt-link" exact to="/">accueil</nuxt-link>
-      </li>
-      <li class="nav__list-item" v-for="route in routes" :key="route">
-        <nuxt-link class="nuxt-link" v-bind:to="route.path">{{route.label}}</nuxt-link>
-      </li>
+        <li class="nav__list-item">
+          <nuxt-link class="nuxt-link" exact to="/">accueil</nuxt-link>
+        </li>
+        <li class="nav__list-item" v-for="route in routes" :key="route">
+          <nuxt-link class="nuxt-link" v-bind:to="route.path">{{route.label}}</nuxt-link>
+        </li>
+
     </ul>
   </nav>
 </template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["page", "projects", "routes", "indexedProject"]),
+    ...mapGetters(["selectedProject"])
+  }
+};
+</script>
 
 <style>
 nav {
@@ -63,13 +75,3 @@ nav {
 }
 </style>
 
-<script>
-import { mapState, mapGetters } from "vuex";
-
-export default {
-  computed: {
-    ...mapState(["page", "projects", "routes", "indexedProject"]),
-    ...mapGetters(["selectedProject"])
-  }
-};
-</script>
